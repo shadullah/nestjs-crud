2f7c836df49ec54564da5dcf61b20abf0491107c
hexaalif <shadullahsad9@gmail.com>
2025-01-23 23:55:23 +0600

    some changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  // Enable validation pipes globally
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed HTTP methods
+    credentials: true, // If you need to include cookies
+  });
 
   await app.listen(process.env.PORT ?? 8000);
 }
